26435c20736e7a566ce8f5c96d216bed9c15bdbf
qypone <qypone@aliyun.com>
2022-02-28 09:24:02 +0800

    二分查找

---
+++ leetcode/src/main/java/com/qypone/leetcode/BinarySearch.java
@@ -0,0 +1,42 @@
+package com.qypone.leetcode;
+
+/**
+ * 给定一个n个元素有序的（升序）整型数组nums 和一个目标值target ，写一个函数搜索nums中的 target，如果目标值存在返回下标，否则返回 -1。
+ * https://leetcode-cn.com/problems/binary-search
+ */
+public class BinarySearch {
+  public static void main(String[] args) {
+    int[] nums = {1, 0, 5};
+
+    int search = new Solution().search(nums, 5);
+    System.out.println(search);
+  }
+}
+
+class Solution {
+  public int search(int[] nums, int target) {
+    return searchOne(nums, nums.length / 2, nums.length, target);
+  }
+
+  private int searchOne(int[] nums, int start_index, int end_index, int target) {
+    if (start_index >= nums.length || end_index == 0) {
+      return -1;
+    }
+    if (start_index == end_index && nums[start_index] != target) {
+      return -1;
+    }
+    if (nums[start_index] == target) {
+      return start_index;
+    }
+    if (nums[end_index - 1] == target) {
+      return end_index - 1;
+    }
+    if (nums[start_index] > target) {
+      return searchOne(nums, start_index / 2, start_index, target);
+    } else if (nums[start_index] < target) {
+      return searchOne(nums, start_index + 1, end_index, target);
+    } else {
+      return start_index;
+    }
+  }
+}
